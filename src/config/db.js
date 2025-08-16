be5071e6c0175972530d49a7cbbaa93cddd82603
DmitriyVeig <veigandt.dm@gmail.com>
2025-08-16 20:19:22 +0300

    Added working dp.js

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+const { Pool } = require('pg');
+const logger = require("../utils/logger");
+const path = require("path");
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+pool.connect(undefined)
+    .then(() =>
+        logger.info("Connected to DB"))
+    .catch((err) =>
+        logger.info("Error: '" + err + "' while trying to connect to DB"));
+module.exports = pool;
