fc60438e5c969334d704612aa28ab2ab81c86b12
DmitriyVeig <veigandt.dm@gmail.com>
2025-08-17 02:32:36 +0300

    Small commit for express middleware

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("./utils/logger");
 const app = express();
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
     res.send(".");
 });
