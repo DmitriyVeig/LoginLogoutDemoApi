303f75ecc1c9ba84b359fe522be4454a79fc7c7b
DmitriyVeig <veigandt.dm@gmail.com>
2025-08-17 12:35:10 +0300

    Added working swagger

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const logger = require("./utils/logger");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+
 const app = express();
 
 app.use(express.json())
 
+const swaggerDocument = YAML.load("./src/docs/swagger.yaml");
+
 app.get("/", (req, res) => {
     res.send(".");
 });
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
