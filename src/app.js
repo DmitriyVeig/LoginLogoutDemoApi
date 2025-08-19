47d5d6c8782e5941ecdf9fa440bbc7ed47ebbc72
DmitriyVeig <veigandt.dm@gmail.com>
2025-08-20 01:08:04 +0300

    Stupid bug

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
-const swaggerDocument = YAML.load("./src/docs/swagger.yaml");
+const swaggerDocument = YAML.load("./docs/swagger.yaml");
 const AuthRoutes = require("./routes/auth.routes");
 const usersRoutes = require("./routes/users.routes");
 
